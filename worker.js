c12e587ce3ff62b2a0390b8acd41ddeab7e6c1a4
svinakraft-maker <svinakraft@gmail.com>
2026-07-13 02:15:23 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,11 @@
 export default {
   async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+    const response = await env.ASSETS.fetch(request);
+   
+    const newResponse = new Response(response.body, response);
+    
+    newResponse.headers.set("content-type", "text/plain; charset=utf-8");
+    
+    return newResponse;
   }
 };
